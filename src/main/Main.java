52ae2ba78a5dd2c7d7e9e4d25edc135b0805066c
Alexander Desyatov <desyatovav@yandex.ru>
2024-01-31 19:12:43 +0300

    Add example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,70 @@
 package main;
 
+import main.model.TaskStatus;
+import main.model.dto.create.CreateEpicDto;
+import main.model.dto.create.CreateSubtaskDto;
+import main.model.dto.create.CreateTaskDto;
+import main.model.dto.response.EpicDto;
+import main.model.dto.response.SubtaskDto;
+import main.model.dto.response.TaskDto;
+import main.model.dto.update.UpdateSubtaskDto;
+import main.repository.TaskRepositoryInMemory;
+import main.service.TaskManagerService;
+import main.util.IdentifierGenerator;
+import main.util.MappingUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        /*
+            Полное тестирование функционала смотрите в тестах
+        */
+
+        TaskManagerService taskManager = new TaskManagerService(
+                new IdentifierGenerator(),
+                new TaskRepositoryInMemory(),
+                new MappingUtils()
+        );
+
+        EpicDto epic = taskManager.createEpic(
+                new CreateEpicDto("Техническое задание", "Технические задание 4 спринта")
+        );
+        SubtaskDto subtask1 = taskManager.createSubtask(
+                new CreateSubtaskDto("Проектирование", "Спроектировать хранение данных"),
+                epic.getId()
+        );
+        SubtaskDto subtask2 = taskManager.createSubtask(
+                new CreateSubtaskDto("Тест", "Написать тесты заглушки"),
+                epic.getId()
+        );
+        SubtaskDto subtask3 = taskManager.createSubtask(
+                new CreateSubtaskDto("Тест", "Написать тесты заглушки"),
+                epic.getId()
+        );
+
+        SubtaskDto subtask4 = taskManager.createSubtask(
+                new CreateSubtaskDto("Код", "Написать код"),
+                epic.getId()
+        );
+
+        System.out.println("Создан новый эпик с подзадачами:");
+        System.out.println(taskManager.getEpic(epic.getId()));
+
+        System.out.println("Создана задача:");
+        TaskDto task = taskManager.createTask(
+                new CreateTaskDto("Опрос", "Пройти опрос после технического задания")
+        );
+        System.out.println(taskManager.getAllTasks());
+
+        System.out.println("Подзадача 1 завершена. Обновленный эпик:");
+        taskManager.updateSubtask(
+                new UpdateSubtaskDto(subtask1.getId(), subtask1.getName(), subtask1.getDescription(), TaskStatus.DONE)
+        );
+        System.out.println(taskManager.getEpic(epic.getId()));
+
+        System.out.println("Удалены все подзадачи подзадачами:");
+        taskManager.removeAllSubtasks();
+        System.out.println(taskManager.getEpic(epic.getId()));
     }
 }
