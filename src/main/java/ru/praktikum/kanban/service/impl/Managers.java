37c98ef06384bb8cb6b63ab140a0025b355ea23d
Alexander Desyatov <desyatovav@yandex.ru>
2024-03-20 22:35:17 +0300

    Restore getDefaultHistory

--- src/main/java/ru/praktikum/kanban/service/impl/Managers.java
+++ src/main/java/ru/praktikum/kanban/service/impl/Managers.java
@@ -1,16 +1,27 @@
 package ru.praktikum.kanban.service.impl;
 
+import ru.praktikum.kanban.repository.HistoryRepository;
+import ru.praktikum.kanban.repository.impl.FileBackedTaskRepository;
+import ru.praktikum.kanban.repository.impl.InMemoryTaskRepository;
 import ru.praktikum.kanban.service.HistoryManager;
 import ru.praktikum.kanban.service.TaskManager;
 import ru.praktikum.kanban.service.backup.TaskFileStorage;
-import ru.praktikum.kanban.repository.impl.FileBackedTaskRepository;
 
 public class Managers {
 
     public static TaskManager getDefault() {
         FileBackedTaskRepository repository = new FileBackedTaskRepository(TaskFileStorage.defaultStorage());
-        HistoryManager historyManager = new HistoryManagerImpl(repository);
+        HistoryManager historyManager = getDefaultHistory(repository);
         return new FileBackedTaskManager(repository, historyManager);
     }
 
+    public static TaskManager getInMemoryTaskManager() {
+        InMemoryTaskRepository repository = new InMemoryTaskRepository();
+        HistoryManager historyManager = getDefaultHistory(repository);
+        return new InMemoryTaskManager(repository, historyManager);
+    }
+
+    public static HistoryManager getDefaultHistory(HistoryRepository repository) {
+        return new HistoryManagerImpl(repository);
+    }
 }
